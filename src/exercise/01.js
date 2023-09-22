d9090cd3fcc2f8775c2a68da23534ed0b3f4b8dd
ostjo <johanna@finmid.com>
2023-09-22 09:21:11 +0200

    add extra credit 04

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,10 +3,21 @@
 
 import * as React from 'react'
 
-const countReducer = (state, action) => ({
-  ...state,
-  ...(typeof action === 'function' ? action(state) : action),
-})
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {...state, count: action.step + state.count}
+    }
+    default: {
+      // helps us avoid typos!
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
@@ -14,13 +25,14 @@ function Counter({initialCount = 0, step = 1}) {
   // const increment = () => changeCount(step)
 
   // Extra credit 02
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
   // const increment = () => setState({count: count + step})
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
